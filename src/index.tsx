410b4091ae639eab381723ff1a7b0e397a69309a
cramer666 <luciaperrone.unlp@gmail.com>
2025-12-18 16:00:08 -0300

    Sigo des y adaptandp para vercel2

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { ThemeProvider } from './context/ThemeContext';
+//import { ThemeProvider } from './context/ThemeContext'; Envolver desp la app
 import './estilos/globals.css';
 import './estilos/animaciones.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider>
       <App />
-    </ThemeProvider>
   </React.StrictMode>
 );
