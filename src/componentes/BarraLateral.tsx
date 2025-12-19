f29bc46c548512b949c103b82c6707864d0bb295
Lucia Perrone <86473981+Cramer666@users.noreply.github.com>
2025-12-19 03:56:55 -0300

    Update BarraLateral.tsx

--- src/componentes/BarraLateral.tsx
+++ src/componentes/BarraLateral.tsx
@@ -13,24 +13,15 @@ const BarraLateral: React.FC = () => {
         border-gray-300 dark:border-gray-800
       "
     >
-      {/* ICONOS SUPERIORES */}
+      {/* ICONOS */}
       <div className="flex flex-col items-center py-4 gap-4">
-        <FileText
-          size={22}
-          className="text-gray-600 dark:text-gray-400 hover:text-black dark:hover:text-white transition cursor-pointer"
-        />
-        <Folder
-          size={22}
-          className="text-gray-600 dark:text-gray-400 hover:text-black dark:hover:text-white transition cursor-pointer"
-        />
-        <Code
-          size={22}
-          className="text-blue-500 dark:text-blue-400"
-        />
+        <FileText className="icono-lateral" size={22} />
+        <Folder className="icono-lateral" size={22} />
+        <Code className="text-blue-500 dark:text-blue-400" size={22} />
       </div>
 
-      {/* ZONA INFERIOR – IDIOMA */}
-      <div className="flex flex-col items-center pb-3">
+      {/* BOTÓN IDIOMA – SOLO DESKTOP */}
+      <div className="hidden md:flex flex-col items-center pb-3">
         <BotonIdioma />
       </div>
     </div>
