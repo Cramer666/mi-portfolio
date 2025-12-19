d152b353c4820ba3eb345db6b0fb54ac802aac76
Lucia Perrone <86473981+Cramer666@users.noreply.github.com>
2025-12-19 04:27:40 -0300

    Create es.ts

---
+++ src/i18n/es.ts
@@ -0,0 +1,17 @@
+const es = {
+  app: {
+    title: 'Portfolio Lucia Perrone',
+  },
+  inicio: {
+    titulo: 'Desarrolladora Full Stack Jr.',
+  },
+  contacto: {
+    titulo: 'Contacto',
+    enviar: 'Enviar Mensaje',
+    enviado: '¡Mensaje enviado con éxito!',
+    error: 'Completá todos los campos',
+    otros: 'También podés contactarme por:',
+  },
+};
+
+export default es;
