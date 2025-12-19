e40a564e0278d9b0b1ab33dee4f0e0f7465050f4
Lucia Perrone <86473981+Cramer666@users.noreply.github.com>
2025-12-19 01:33:16 -0300

    Change CSS import from index.css to global.css

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
+import './estilos/global.css';
 import { ThemeProvider } from './context/ThemeContext';
 
 const rootElement = document.getElementById('root');
