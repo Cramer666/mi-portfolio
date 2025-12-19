7a64bebf2da5d1a44196649bd931333baed2f2e8
Lucia Perrone <86473981+Cramer666@users.noreply.github.com>
2025-12-19 01:44:15 -0300

    Update BarraLateral styles for improved appearance

--- src/componentes/BarraLateral.tsx
+++ src/componentes/BarraLateral.tsx
@@ -3,7 +3,7 @@ import { FileText, Folder, Code } from 'lucide-react';
 
 const BarraLateral: React.FC = () => {
   return (
-    <div className="hidden md:flex w-12 bg-[#333333] flex-col items-center py-4 gap-4 border-r border-gray-800">
+    <div className="hidden md:flex w-12 flex-col items-center py-4 gap-4 border-r bg-gray-200 dark:bg-[#333333] border-gray-300 dark:border-gray-800 ">
       <FileText className="text-gray-400 hover:text-white cursor-pointer transition" size={24} />
       <Folder className="text-gray-400 hover:text-white cursor-pointer transition" size={24} />
       <Code className="text-blue-400" size={24} />
