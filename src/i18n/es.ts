c8a18b2b3af5270b766b129c131ae010aef03e0e
Lucia Perrone <86473981+Cramer666@users.noreply.github.com>
2025-12-19 04:46:50 -0300

    Update es.ts

--- src/i18n/es.ts
+++ src/i18n/es.ts
@@ -6,13 +6,19 @@ const es = {
     titulo: 'Desarrolladora Full Stack Jr.',
   },
   contacto: {
-    titulo: 'Contacto',
+    asyncFn: 'async function',
+    nombreFn: 'enviarMensaje',
+    formulario: 'formulario',
+    nombre: 'nombre',
+    email: 'email',
+    mensaje: 'mensaje',
     enviar: 'Enviar mensaje',
     enviando: 'Enviando...',
     enviado: '¡Mensaje enviado con éxito!',
-    error: 'Completá todos los campos',
+    error: 'Completá todos los campos.',
     otros: 'También podés contactarme directamente por:',
-  },
+  }
+
 };
 
 export default es;
